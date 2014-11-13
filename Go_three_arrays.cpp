fc53dcfeef2191145b229fc997aafe716df5d77a
nikox94 <nikox94@users.noreply.github.com>
2014-11-13 19:00:15 +0200

    Update Go_three_arrays.cpp
    Zadachki

--- Go_three_arrays.cpp
+++ Go_three_arrays.cpp
@@ -1,25 +1,79 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
 
-
-    char m[20];
-    char k;
-    for(int i=0;i<20;i++)
-    {
-        cin>>m[i];
-    }
-    bool flag=true;
-    do
-    {
-    for(int i=0;i<20;i++)
-    {
-        if(m[i]>m[i+1])
-        {
-            k=m[i];
-            m[i]=m[i+1];
-            m[i+1]=k;
-            flag=true;
+int main()
+{
+    //We will switch on the problem number so that we can
+    //save ourselves many different short files and combine them into one
+    int problem_no = 4;
+    switch(problem_no){
+case 1:
+    break;
+case 2:
+//    int N;
+//    cin>>N;
+//    int a[N];
+//    int mini = 500;
+//    int maxi=0;
+//    for(int i=0; i<N;++i){
+//        cin>>a[i];
+//        if(mini>a[i])
+//            mini = a[i];
+//        if(maxi<a[i])
+//            maxi = a[i];
+//    }
+//    cout<<mini<<endl;
+//    cout<<maxi<<endl;
+//
+    break;
+case 3:
+//    int N;
+//    int sum=0;
+//    cin>>N;
+//    int a[N];
+//    for(int i=0;i<N;i++){
+//        cin>>a[i];
+//        sum+=a[i];
+//    }
+//    cout<<"Your GPA="<<(double)sum/N<<endl;
+    break;
+case 4:
+    int n;
+    cin>>n;
+    int a[n];
+    bool flag=false;
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+        if(i>0){
+            if(a[i-1]>a[i]){
+                cout<<"Is not sorted!!!";
+                flag = true;
+                break;
+            }
         }
     }
+    if (!flag){
+        cout<<"Is sorted"<<endl;
     }
-    while(flag);
-        
+
+    //for (int i=0;i<n;i++) cin>>a[i];
+    /*int j=0;
+    while (j!=n-2)
+    {
+        if (a[j]<=a[j+1])
+            j++;
+        else
+        {
+            cout<<"Not sorted"<<endl;
+            break;
+        }
+        if (a[n-2]<=a[n-1])
+            cout<<"Sorted"<<endl;
+        else
+            cout<<"Not sorted";
+    }*/
+
+    break;
+    }
+    return 0;
+}
