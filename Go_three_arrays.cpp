75529dd08951bfd2d3a541cf2557b39ad0fea18f
nikox94 <nikox94@users.noreply.github.com>
2014-11-13 18:20:45 +0200

    Update Go_three_arrays.cpp

--- Go_three_arrays.cpp
+++ Go_three_arrays.cpp
@@ -1,1 +1,25 @@
 #include<iostream>
+
+
+    char m[20];
+    char k;
+    for(int i=0;i<20;i++)
+    {
+        cin>>m[i];
+    }
+    bool flag=true;
+    do
+    {
+    for(int i=0;i<20;i++)
+    {
+        if(m[i]>m[i+1])
+        {
+            k=m[i];
+            m[i]=m[i+1];
+            m[i+1]=k;
+            flag=true;
+        }
+    }
+    }
+    while(flag);
+        
